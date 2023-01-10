f69d8d4f3a1543f2d5842fc013aee9a638f3bc59
ozen <bluefirt15@gmail.com>
2023-01-10 14:12:44 -0500

    add pwa

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,27 @@
+import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<meta charSet="utf-8" />
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+				<meta
+					name="viewport"
+					content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+				/>
+				<meta name="description" content="Description" />
+				<meta name="keywords" content="Keywords" />
+				<title>My links</title>
+
+				<link rel="manifest" href="/manifest.json" />
+	
+				<meta name="theme-color" content="#0f0f0f" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
