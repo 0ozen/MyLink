bc074fdbc9f9b85e0d44960936eb82cef9c89b75
ozen <bluefirt15@gmail.com>
2023-03-13 08:22:37 -0500

    fix: indentacion

--- src/pages/api/editUrl.js
+++ src/pages/api/editUrl.js
@@ -1,32 +1,32 @@
-import dbConnect from "../../utils/mongoose";
-import Url from "../../models/link";
+import dbConnect from '../../utils/mongoose';
+import Url from '../../models/link';
 
 export default async function handler(req, res) {
-	if (req.body.link === "")
-		return res.status(400).json("Ingrese una url valida");
+  if (req.body.link === '')
+    return res.status(400).json('Ingrese una url valida');
 
-	try {
-		await dbConnect();
-		let link = req.body.link;
-		if (link.search(/^http[s]?\:\/\//) == -1) {
-			link = "https://" + link;
-		}
-		const data = await Url.findOne({ urlOriginal: link });
+  try {
+    await dbConnect();
+    let link = req.body.link;
+    if (link.search(/^http[s]?\:\/\//) == -1) {
+      link = 'https://' + link;
+    }
+    const data = await Url.findOne({ urlOriginal: link });
 
-		if (data) {
-			data.customUrl = req.body.edit;
-			await data.save();
-			return res.status(201).send(data.customUrl);
-		}
+    if (data) {
+      data.customUrl = req.body.edit;
+      await data.save();
+      return res.status(201).send(data.customUrl);
+    }
 
-		const newUrl = await new Url({ urlOriginal: link });
-		newUrl.customUrl = req.body.edit;
+    const newUrl = await new Url({ urlOriginal: link });
+    newUrl.customUrl = req.body.edit;
 
-		await newUrl.save();
+    await newUrl.save();
 
-		return res.status(201).send(newUrl.customUrl);
-	} catch (error) {
-		console.log(error.message);
-		return res.status(500).send(error);
-	}
+    return res.status(201).send(newUrl.customUrl);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send(error);
+  }
 }
--- src/pages/api/shortUrl.js
+++ src/pages/api/shortUrl.js
@@ -1,40 +1,40 @@
-import dbConnect from "../../utils/mongoose";
-import Url from "../../models/link";
-import { nanoid } from "nanoid";
+import dbConnect from '../../utils/mongoose';
+import Url from '../../models/link';
+import { nanoid } from 'nanoid';
 
 export default async function handler(req, res) {
-	let link = req.body.link;
+  let link = req.body.link;
 
-	if (link.search(/^http[s]?\:\/\//) == -1) {
-		link = "https://" + link;
-	}
+  if (link.search(/^http[s]?\:\/\//) == -1) {
+    link = 'https://' + link;
+  }
 
-	const isValidUrl = (urlString) => {
-		try {
-			return Boolean(new URL(urlString));
-		} catch (e) {
-			return false;
-		}
-	};
+  const isValidUrl = (urlString) => {
+    try {
+      return Boolean(new URL(urlString));
+    } catch (e) {
+      return false;
+    }
+  };
 
-	if (!isValidUrl(link)) return res.status(400).json("Ingrese una url valida");
+  if (!isValidUrl(link)) return res.status(400).json('Ingrese una url valida');
 
-	try {
-		await dbConnect();
+  try {
+    await dbConnect();
 
-		const data = await Url.findOne({ urlOriginal: link });
+    const data = await Url.findOne({ urlOriginal: link });
 
-		if (data) {
-			data.shortUrl = nanoid(7);
-			await data.save();
-			return res.status(201).send(data.shortUrl);
-		}
+    if (data) {
+      data.shortUrl = nanoid(7);
+      await data.save();
+      return res.status(201).send(data.shortUrl);
+    }
 
-		const newUrl = await new Url({ urlOriginal: link });
-		await newUrl.save();
-		return res.status(201).send(newUrl.shortUrl);
-	} catch (error) {
-		console.log(error.message);
-		return res.status(500).send(error);
-	}
+    const newUrl = await new Url({ urlOriginal: link });
+    await newUrl.save();
+    return res.status(201).send(newUrl.shortUrl);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send(error);
+  }
 }
