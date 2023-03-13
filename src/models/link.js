de1a4dc6049cf839ab49dbaa86562cb0500d3323
ozen <bluefirt15@gmail.com>
2023-03-13 07:57:19 -0500

    fix: error con la indentacion

--- src/models/link.js
+++ src/models/link.js
@@ -1,33 +1,33 @@
-import mongoose from "mongoose";
-import { nanoid } from "nanoid";
+import mongoose from 'mongoose';
+import { nanoid } from 'nanoid';
 
 const UrlSchema = new mongoose.Schema({
-	urlOriginal: {
-		type: String,
-		unique: true,
-		trim: true,
-		required: [true, "Ingrese una url valida."],
-		maxlength: [150, "url cannot be more than 150 characters"],
-	},
-	shortUrl: {
-		type: String,
-		unique: true,
-		default: () => nanoid(7),
-		maxlength: [20, "cannot be more than 20 characters"],
-	},
-	customUrl: {
-		type: String,
-		sparse: true,
-		unique: true,
-		trim: true,
-		maxlength: [30, "cannot be more than 30 characters"],
-	},
-	clicks: {
-		required: true,
-		type: Number,
-		default: 0,
-		min: 0,
-	},
+  urlOriginal: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: [true, 'Ingrese una url valida.'],
+    maxlength: [150, 'url cannot be more than 150 characters'],
+  },
+  shortUrl: {
+    type: String,
+    unique: true,
+    default: () => nanoid(7),
+    maxlength: [20, 'cannot be more than 20 characters'],
+  },
+  customUrl: {
+    type: String,
+    sparse: true,
+    unique: true,
+    trim: true,
+    maxlength: [30, 'cannot be more than 30 characters'],
+  },
+  clicks: {
+    required: true,
+    type: Number,
+    default: 0,
+    min: 0,
+  },
 });
 
-export default mongoose.models?.Url || mongoose.model("Url", UrlSchema);
+export default mongoose.models?.Url || mongoose.model('Url', UrlSchema);
